ce4e11882c947f96e45377b38a730f647c5bd323
Adrian <92938856+agax1210@users.noreply.github.com>
2025-11-01 10:45:46 +0100

    Remove duplicate UnicodeSymbol property in Queen class

--- Shared/Pieces/Queen.cs
+++ Shared/Pieces/Queen.cs
@@ -3,6 +3,4 @@ namespace Shared.Pieces;
 public class Queen(string position, bool isWhite) : Piece(position, "Queen", isWhite)
 {
     public override string UnicodeSymbol => "♛";
-    
-    public override string UnicodeSymbol => IsWhite ? "\u2655" : "\u265B";
 }
