e210bef5685f529a517c114d71a735ea0d5ad85c
Lucam31 <122787364+Lucam31@users.noreply.github.com>
2025-11-03 09:12:05 +0100

    Feature/validator (#4)
    * Add basic Console Functions
    
    * Add Validation for move input and basic moves
    
    * Update MoveValidator
    
    * Remove duplicate UnicodeSymbol property in Queen class
    
    * Fix Move Validation and some CLI Output
    
    ---------
    
    Co-authored-by: Adrian <92938856+agax1210@users.noreply.github.com>

--- chess-client/InputParser.cs
+++ chess-client/InputParser.cs
@@ -1,4 +1,5 @@
 namespace chess_client;
+using Shared;
 
 public static class InputParser
 {
@@ -11,7 +12,7 @@ public static class InputParser
 
         if (prompt == "")
         {
-            throw new ArgumentException("prompt must not be empty.");
+            CLIOutput.WriteErrorMessage("prompt must not be empty.");
         }
 
         while (true)
@@ -33,20 +34,20 @@ public static class InputParser
         }
     }
 
-    public static string ReadMove()
+    public static Move ReadMove()
     {
         int fromCol, fromRow, toCol, toRow = -1;
         string? move = Console.ReadLine()?.Trim().ToUpper();
         if (move?.Length < 4 || move?.Length > 4)
         {
-            throw new ArgumentException("Input must be in the format 'e2e4'.");
+            CLIOutput.WriteErrorMessage("Input must be in the format 'e2e4'.");
         }
         try
         {
-            fromRow = move[0];
-            fromCol = move[1];
-            toRow = move[2];
-            toCol = move[3];
+            fromCol = move[0];
+            fromRow = move[1]-'0';
+            toCol = move[2];
+            toRow = move[3]-'0';
         }
         catch (Exception e)
         {
@@ -56,14 +57,14 @@ public static class InputParser
 
         if (fromRow == -1 || fromCol == -1 || toRow == -1 || toCol == -1)
         {
-            throw new ArgumentException("Input must be in the format 'e2e4'.");
+            CLIOutput.WriteErrorMessage("Input must be in the format 'e2e4'.");
         }
 
-        if (fromRow is > 64 and < 73 && fromCol is > 0 and < 9 && toRow is > 64 and < 73 && toCol is > 0 and < 9)
+        if (fromCol is > 64 and < 73 && fromRow is > 0 and < 9 && toCol is > 64 and < 73 && toRow is > 0 and < 9)
         {
-            return move;
+            return new Move($"{Convert.ToChar(fromCol)}{fromRow}",$"{Convert.ToChar(toCol)}{toRow}");
         }
 
-        throw new ArgumentException("Input must be between A-H and 1-8.");
+        throw new ArgumentException("Input was not valid");
     }
 }
